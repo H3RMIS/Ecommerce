dc4e165ddb7259215a0d27d8e8076fe814b6677f
Ko3ch <tomko3ch@gmail.com>
2022-09-30 12:34:14 +0300

    created product schema

---
+++ sanityecommerce/schemas/product.js
@@ -0,0 +1,44 @@
+// export schema as a basic javascript object
+export default{
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'array',
+            of: [{type: 'image'}],
+            options: {
+                hotspot: true, 
+            }
+            // hotspot is Sanity image property 
+            // that enables selecting of the area of the image that should cropped or resized
+            // or better position the image when uploaded
+        },
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 90,
+            }
+        },
+        {
+            name: 'price',
+            title: 'Price',
+            type: 'number',
+        },
+        {
+            name: 'details',
+            title: 'Details',
+            type: 'string',
+        }
+    ]
+}
