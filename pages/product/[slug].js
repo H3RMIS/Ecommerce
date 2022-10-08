b4b8eb41eb383c7a25a2eb9bbc46eae890ec6acb
Ko3ch <tomko3ch@gmail.com>
2022-10-09 00:28:19 +0300

    added similar products section

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -58,7 +58,16 @@ const ProductDetails = ({ product, products }) => {
             </div>
         </div>
 
-        
+        <div className='maylike-products-wrapper'>
+            <h2>You may also like</h2>
+            <div className='marquee'>
+                <div className='maylike-products-container track'>
+                    {products.map((item) => (
+                        <Product key={item._id} product={item}/>
+                    ))}
+                </div>
+            </div>
+        </div>
 
     </div>
   )
