4f5233b06a9e195e8e028829aceecf71676e8d7b
Ko3ch <tomko3ch@gmail.com>
2022-10-09 00:27:20 +0300

    added product image ,description and call to action buttons

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -3,43 +3,97 @@
 
 import React from 'react'
 import { client, urlFor } from '../../lib/client'
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import { Product } from  '../../components';
 
+const ProductDetails = ({ product, products }) => {
+    
+    const{ image, name, details, price } = product;   
 
-const ProductDetails = () => {
   return (
     <div>
         <div className='product-detail-container'>
             <div>
                 <div className='image-container'>
-                    <img src=''/>
+                    <img src={urlFor(image && image[0])}/>
+                </div>
+                {/* <div className="small-images-container">
+                    {image?.map((item, i) => (
+                        <img 
+                        src={urlFor(item)}
+                        className=""
+                        onMouseEnter=""
+                        />
+                    ))}
+                </div> */}
+            </div>
+            <div className="product-detail-desc">
+                <h1>{name}</h1>
+                <div className='reviews'>
+                    <div>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiFillStar/>
+                        <AiOutlineStar/>
+                    </div>
+                    <p>(30)</p>
+                </div>
+                <h4>Details</h4>
+                <p>{details}</p>
+                <p className='price'>${price}</p>
+                <div className='quantity'>
+                    <h3>Quantity: </h3>
+                    <p className='quantity-desc'>
+                        <span className='minus' onClick=""><AiOutlineMinus/></span>
+                        <span className='num' onClick="">0</span>
+                        <span className='plus' onClick=""><AiOutlinePlus/></span>
+                    </p>
+                </div>
+
+                <div className='buttons'>
+                    <button type='button' className='add-to-cart' onClick="">Add to Cart</button>
+                    <button type='button' className='buy-now' onClick="">Buy Now</button>
                 </div>
             </div>
-        </div>    
+        </div>
+
+        
+
     </div>
   )
 }
-
+export const getStaticPaths = async () => {
+    const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }`;//getting current property of a slug of each of the products 
+    const products = await client.fetch(query);
+    //to generate the paths and instantly returning an object
+    const paths = products.map((product) => ({
+        params: {
+            slug: product.slug.current
+        }
+    }));
+    return {
+        paths,
+        fallback: 'blocking'
+    }
+}
 export const getStaticProps = async ({ params: { slug } }) => {
-
-    //getStaticProps is used if the data required to render the page is available 
-    //at build time ahead of a users request
-    //if the data comes from a headless CMS
-    //If we are on the home page the user can click on any of the product images,,so the data having been already store
-    //in Sanity CMS , everything will be populated instantly
-    //We can also destructure params and get access to the query
-    
+    //getStaticProps is used if the data required to render the page is available at build time ahead of a users request. if the data comes from a headless CMS
+    //If we are on the home page the user can click on any of the product images,,so the data having been already store //in Sanity CMS , everything will be populated instantly . We can also destructure params and get access to the query
     const query = `*[_type == "product" && slug.current == '${slug}'][0]`; //fetch product details from the product page that we are on
-    const products = await client.fetch(query); 
-  
-    
+    //[0] we only want to fetch the first product that matches this query
+    const productsQuery = '*[_type == "product"]'
+    const product = await client.fetch(query); 
+    const products = await client.fetch(productsQuery);
 
-    const bannerQuery = '*[_type == "banner"]';
-    const bannerData = await client.fetch(bannerQuery);
-  
     return {
       props: {
+        product,
         products,
-        bannerData
       }
     }
 }
