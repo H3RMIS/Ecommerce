e155d665bc680e6ea3dbf8da5a7121ed028ba594
Ko3ch <tomko3ch@gmail.com>
2022-10-09 12:39:56 +0300

    imported react hooks for the logic

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,1 +1,3 @@
 // file to manage the entire state of the application
+//import react hooks
+import React, { createContext, useContext, useState, useEffect } from 'react';
--- context/statecontext.js
+++
@@ -1,1 +0,0 @@
-// file to manage the entire state of the application
