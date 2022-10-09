4684cb8e7affd00808010ea7d76a12b290c563d4
Ko3ch <tomko3ch@gmail.com>
2022-10-09 16:19:49 +0300

    added the states that will be used in the app

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,4 +1,17 @@
 // file to manage the entire state of the application
 //import react hooks
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { toast } from 'react-hot-toast'; 
+import { toast } from 'react-hot-toast'; //imported toast for pop up notification when item is added to cart
+
+const Context = createContext(); //call as hook
+export const StateContext = ( { children } ) => {
+    //create the states
+    const [showCart, setshowCart] = useState(false); //manage the state of the cart if it's being shown or not
+    const [cartItems, setcartItems] = useState() //to know what items are in the cart. Will be filled with data coming from local storage
+    //when user leaves site, the items will no get erased.
+    const [totalPrice, settotalPrice] = useState() //keep track of the total price 
+    const [totalQuantities, settotalQuantities] = useState() //to know the quantities of the items you are working with
+    const [qty, setqty] = useState(1)
+    
+}
+
