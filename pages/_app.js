5729868a4e6262f3fb2304ca9db768477fff2a95
Ko3ch <tomko3ch@gmail.com>
2022-10-09 17:08:01 +0300

    added Toaster as a self-closing component for pop-up notifications

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import { Toaster } from 'react-hot-toast' //for notification pop up
 import '../styles/globals.css'
+import Layout from '../components/Layout'
 
+import { StateContext } from '../context/StateContext' 
+//to implement accessing the states in the entire application by wrapping the entire app with StateContext
+ //to pass the data to every single component if need be
+
+ 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( 
+  <StateContext>
+    <Layout>
+      <Toaster/>
+      <Component {...pageProps} />
+    </Layout>
+  </StateContext>
+  )
 }
 
 export default MyApp
+
