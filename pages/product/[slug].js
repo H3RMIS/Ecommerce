4d5b87a94d355ec88d9405f249dfe11d8e8cf98e
Ko3ch <tomko3ch@gmail.com>
2022-10-11 00:24:10 +0300

    destructure onAdd function and called it on the Add to Cart button and passed the parameters product and qty to execute

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -11,7 +11,7 @@ const ProductDetails = ({ product, products }) => {
     
     const{ image, name, details, price } = product;   
     const [index, setIndex] = useState(0);
-    const { decQty, incQty, qty } = useSateContext();
+    const { decQty, incQty, qty, onAdd } = useSateContext();
 
   return (
     <div>
@@ -55,7 +55,7 @@ const ProductDetails = ({ product, products }) => {
                 </div>
 
                 <div className='buttons'>
-                    <button type='button' className='add-to-cart' onClick="">Add to Cart</button>
+                    <button type='button' className='add-to-cart' onClick={() => onAdd(product, qty)}>Add to Cart</button>
                     <button type='button' className='buy-now' onClick="">Buy Now</button>
                 </div>
             </div>
