e5f59dee7037ec9c9391359c4a618ade5ac830b8
Ko3ch <tomko3ch@gmail.com>
2022-10-11 01:59:22 +0300

    implement setShowCart and showCart only when the button is clicked

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,18 +5,19 @@ import { Cart } from './';
 import { useSateContext } from '../context/StateContext';
 
 const Navbar = () => {
-   
+  const { showCart, setShowCart, totalQuantities } = useSateContext();
   return (
     <div className='navbar-container'>
       <p className='logo'>
         <Link href='/'>H3RMES Store</Link>
       </p>
-      <button type='button' className='cart-icon' onClick="">
+      <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
         <AiOutlineShopping/>
         <span className='cart-item-qty'>
         </span>
       </button>
-      <Cart/>
+      
+      {showCart && <Cart/>}
     </div>
   )
 }
