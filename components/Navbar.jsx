2dff8162784ae32a44befcc459ad3af832aa1ac4
Ko3ch <tomko3ch@gmail.com>
2022-10-11 02:14:00 +0300

    called the function setShowCart to open and close the cart on demand

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,22 +2,26 @@ import React from 'react'
 import Link from 'next/link'
 import { AiOutlineShopping } from 'react-icons/ai' //shopping icon
 import { Cart } from './';
-import { useSateContext } from '../context/StateContext';
+import { useStateContext } from '../context/StateContext';
 
 const Navbar = () => {
-  const { showCart, setShowCart, totalQuantities } = useSateContext();
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
   return (
+
     <div className='navbar-container'>
       <p className='logo'>
         <Link href='/'>H3RMES Store</Link>
       </p>
+
       <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
         <AiOutlineShopping/>
         <span className='cart-item-qty'>
+          {totalQuantities}
         </span>
       </button>
       
       {showCart && <Cart/>}
+
     </div>
   )
 }
