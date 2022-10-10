a330c9d14d704432e7d63f38a9238c6174c5823d
Ko3ch <tomko3ch@gmail.com>
2022-10-10 17:25:30 +0300

    created functions to handle updating quantity of item

--- context/StateContext.js
+++ context/StateContext.js
@@ -14,7 +14,21 @@ export const StateContext = ( { children } ) => {
     const [qty, setqty] = useState(1)
 
     // return ContextProvider wrapping the values(state fields) that will be passed in the entire application
-    //The values passed can be accessed from any of the components
+    //The values passed can be accessed from any of the componentsci
+
+    //dynamic update quantity function to manage incrementing items 
+    const incQty = () => {
+        setQty(( prevQty ) => prevQty + 1);
+    }
+    const decQty = () => {
+        setQty((prevQty) => {
+            if(prevQty - 1 < 1) return 1;
+
+            return prevQty -1
+        });
+    }
+
+
     return (
         <Context.Provider
         value={{
@@ -23,7 +37,9 @@ export const StateContext = ( { children } ) => {
             totalPrice,
             totalPrice,
             totalQuantities,
-            qty
+            qty,
+            incQty,
+            decQty,
         }}
         >  
             {children}
