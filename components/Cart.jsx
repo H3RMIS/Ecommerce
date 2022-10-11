7f1b5607feed168fb7a6262ed605641e54b495d2
Ko3ch <tomko3ch@gmail.com>
2022-10-11 10:38:48 +0300

    import useStateContext to manage states of products in cart

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
 import { TiDeleteOutline } from 'react-icons/ti'
 import { toast } from 'react-hot-toast';
+import { useStateContext } from '../context/StateContext';
+
 
 const Cart = () => {
   return (
