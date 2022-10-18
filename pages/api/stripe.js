ef15df21a57f18f35ac644c5936a38ea0176cc17
Ko3ch <tomko3ch@gmail.com>
2022-10-18 09:47:23 +0300

    create a checkout session and return 200 status status code if successful

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
       }
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+      res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
