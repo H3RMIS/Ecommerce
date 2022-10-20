4bf2b20a7c853e08cbd786d27d4826c063d3da94
Ko3ch <tomko3ch@gmail.com>
2022-10-20 09:38:00 +0300

    created success message section

---
+++ pages/success.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link';
+import { BsBagCheckFill } from 'react-icons/bs';
+
+
+import { useStateContext } from '../context/StateContext';
+
+
+const Success = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantites } = useStateContext();
+  
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className='icon'>
+          <BsBagCheckFill/>
+        </p>
+        <h2>Thank you for you order!</h2>
+        <p className='email-msg'>Check your email inbox for the receipt.</p>
+        <p className="description">For any inquiries, please email
+        <a className='email' href='mailto:tom.k.kipkurui@gmail.com'></a>
+        </p>
+
+        <Link href="/">
+          <button type='button' width="300px" className='btn'>
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Success
