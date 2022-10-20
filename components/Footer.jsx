038554e040e560b36aeaac3fcedf5c38b9a8c40f
Ko3ch <tomko3ch@gmail.com>
2022-10-20 15:15:27 +0300

    added links to socials

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import { AiOutlineTwitter, AiFillLinkedin, AiFillGithub } from 'react-icons/ai'
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 
 const Footer = () => {
   return (
     <div className='footer-container'>
       <p>2022 HERMES_LOUD All rights reserved</p>
       <p className="icons">
-        <a href="https://www.linkedin.com/in/kko3ch/"><AiFillLinkedin/></a>
-        <a href="https://twitter.com/kko3ch"><AiOutlineTwitter/></a>
-        <a href="https://github.com/kko3ch"><AiFillGithub/></a>
+        <AiFillInstagram/>
+        <AiOutlineTwitter/>
       </p>
     </div>
   )
