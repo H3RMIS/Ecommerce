b17db923fa46b7377ce43f58650498999890438e
Ko3ch <tomko3ch@gmail.com>
2022-10-20 09:48:54 +0300

    cleared sessions data and reinitialized data to default values

--- pages/success.js
+++ pages/success.js
@@ -8,6 +8,13 @@ import { useStateContext } from '../context/StateContext';
 
 const Success = () => {
   const { setCartItems, setTotalPrice, setTotalQuantites } = useStateContext();
+
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantites(0);
+  },[]);
   
   return (
     <div className="success-wrapper">
